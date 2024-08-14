c1c82ccdee27f89fad63f29b1757bdc5856b12e0
Umut Karaytu <128383118+Karaytumut20@users.noreply.github.com>
2024-08-14 20:52:09 +0300

    d

--- app/components/general/Heading.tsx
+++ app/components/general/Heading.tsx
@@ -1,11 +1,16 @@
-interface HeadingProps {
-    conter?: boolean
-    text: string
-}
-const Heading:React.FC<HeadingProps> = ({center,text}) => {
-  return (
-    <div className={`text-slate-500 my-3 md:my-10 px-3 md:px-10 md:text-xl  ${center ? "text-center": "text-start"}`}>{text}</div>
-  )
-}
+import React from 'react';
 
-export default Heading
+export type HeadingProps = {
+    text: string;
+    center?: boolean; // "center" özelliğini ekledik ve opsiyonel yaptık.
+};
+
+const Heading: React.FC<HeadingProps> = ({ center, text }) => {
+    return (
+        <div className={`text-slate-500 my-3 md:my-10 px-3 md:px-10 md:text-xl ${center ? "text-center" : "text-start"}`}>
+            {text}
+        </div>
+    );
+};
+
+export default Heading;
